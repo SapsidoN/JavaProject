b5e1817ffe991c6d29d1eac8af7383ab65b09988
Oleg <oleg33367@mail.ru>
2026-09-16 20:58:22 +0300

    task 1.1.3 v 0.2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,7 +8,7 @@ public class Util {
     // реализуйте настройку соеденения с БД
     private static final String url = "jdbc:mysql://localhost:3306/mydb";
     private static final String user = "root";
-    private static final String password = "5944200Aa";
+    private static final String password = "password";
 
 
     public static Connection Connection () throws SQLException {
